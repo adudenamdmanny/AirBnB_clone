81f6579118cc9dc87cc198995735b4af474af8e8
Emmanuel Essien <117782578+adudenamdmanny@users.noreply.github.com>
2023-05-14 19:24:55 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
-
-"""
-This module creates a unique FileStorage instance for our application.
-Using the Method "reload", it deserializes the JSON file i.e it converts
-the string dictionary of the classs in json file back to and actual dictionary.
-It then convert the dictionary back to an instance, map
-<Class.id> to the <instance> and save all in storage object (dict or {}).
-This happens each time module model is imported, just to ensure we have
-updated instances to work with.
-"""
+"""Initializes the package"""
 from models.engine.file_storage import FileStorage
-
 storage = FileStorage()
 storage.reload()
